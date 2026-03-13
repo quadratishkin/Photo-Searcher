c17d1ecd48eefe635251e04da039ee2d4e3a52a9
Никита <nikitavolkov.volk@mail.ru>
2026-03-14 04:49:29 +0500

    change deep-translator instead of googleTrans

--- backend/searcher_app/translator.py
+++ backend/searcher_app/translator.py
@@ -1,25 +1,27 @@
-from googletrans import Translator as GoogleTranslator
+from deep_translator import GoogleTranslator
 from typing import Optional
 
 class Translator:
     """
-    Класс для перевода текста  с английского на русский с помощью Google Translate.
+    Класс для перевода текста с английского на русский.
     """
-
+    
     def __init__(self):
-        self.translator = GoogleTranslator()
-
-    def translate(self, text: str, dest: str = "ru", src: str = "en") -> Optional[str]:
+        self.translator = GoogleTranslator(source='en', target='ru')
+    
+    def translate(self, text: str) -> Optional[str]:
         """
         Переводит текст с английского на русский.
         :param text: исходный текст
-        :param dest: целевой язык (по умолчанию русский)
-        :param src: исходный язык (по умолчанию английский)
         :return: переведённый текст или None в случае ошибки
         """
         try:
-            result = self.translator.translate(text, src=src, dest=dest)
-            return result.text
+            return self.translator.translate(text)
         except Exception as e:
             print(f"[Translator] Ошибка перевода: {e}")
             return None
+
+# Использование
+trans = Translator()
+result = trans.translate("hello world")
+print(result)
